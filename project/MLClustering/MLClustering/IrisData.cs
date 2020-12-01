4adb741e459e546c3ca8cf8d7019b232db5c83a2
shivanisriup <47216897+shivanisriup@users.noreply.github.com>
2020-12-01 11:47:05 +0530

    Iris_Data_Dotnet_Project

---
+++ project/MLClustering/MLClustering/IrisData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+
+namespace MLClustering
+{
+    public class IrisData
+    {
+        [LoadColumn(0)]
+        public float sepal_length;
+
+        [LoadColumn(1)]
+        public float sepal_width;
+
+        [LoadColumn(2)]
+        public float petal_length;
+
+        [LoadColumn(3)]
+        public float petal_width;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
